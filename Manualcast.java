76ebbab8661efdde731b1e73adeff9e276ed5cdb
abrar <abrar7.md@outlook.com>
2025-08-04 10:16:21 +0530

    Add files via upload

---
+++ Manualcast.java
@@ -0,0 +1,11 @@
+public class manualcast {
+  public static void main (String[] args) 
+  {
+    double d = 9.78;
+    int i = (int) d; 
+// Manual casting: double to int
+
+    System.out.println(d);   
+    System.out.println(i);      
+  }
+}
